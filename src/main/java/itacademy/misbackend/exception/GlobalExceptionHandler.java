d12ae504fa15e641a013570a499430d6205d750d
Shirin Kydralieva <kydralievashirin1@gmail.com>
2024-06-18 12:55:26 +0600

    Изменила в GlobalExceptionHandler @ControllerAdvice на @RestControllerAdvice

--- src/main/java/itacademy/misbackend/exception/GlobalExceptionHandler.java
+++ src/main/java/itacademy/misbackend/exception/GlobalExceptionHandler.java
@@ -4,15 +4,12 @@ import itacademy.misbackend.dto.CustomResponseMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.stream.Collectors;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
